26d73ac338414ce69829703ba44cf86e1d1bd39a
Korben3 <42899281+Korben3@users.noreply.github.com>
2018-09-16 23:58:37 +0200

    Update liskwall.js
    Added emoticons, increased messages to 12, removed flickering.

--- liskwall.js
+++ liskwall.js
@@ -1,24 +1,48 @@
+// liskwall by korben3 - sept-2018 - V0.2
+
 $(document).ready(function() {
   init();
 });
 
+var timestamp; 
+
 function init(){
-	//retrieve message after the page is loaded and setInterval to retrieve any new message each x seconds
+	//retrieve message after the page is loaded and setInterval to retrieve any new 
 	retrieveMessages()
-	setInterval(retrieveMessages,10000);	
+	setInterval(retrieveMessages,10000); 
 };
 
-function retrieveMessages(){
-//retrieve data fields content from the last 10 transactions from an address, and make html code save to display.
-const testnetClient=lisk.APIClient.createTestnetAPIClient();
-$("#messages").html("");
-testnetClient.transactions.get({recipientId:'7134531444980378449L', limit:10, offset:0, sort: 'timestamp:desc' })
-	.then(output => { 
+function convertTimestamp(timestampOld){
+	var months=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
 	
-			for (var i = 9; i >= 0; i--) {
-				var fixedData=(output.data[i].asset.data);
-					fixedData=fixedData.replace(/>/g, "&gt;").replace(/</g, "&lt;");
-					$("#messages").append("<p>"+fixedData+"</p>");
+	const epoch=lisk.constants.EPOCH_TIME;
+	var d=new Date((((Date.parse(epoch)/1000)+timestampOld)*1000));
+    timestampNew=d.getDate()+" "+ months[(d.getMonth())]+" "+d.getHours()+':'+d.getMinutes();
+	return timestampNew;
+}
+
+function retrieveMessages(){
+	//retrieve data fields content from the last 10 transactions from an address, and make html code safe to display.
+	const testnetClient=lisk.APIClient.createTestnetAPIClient();
+	//$("#messages").html("");
+
+	testnetClient.transactions.get({recipientId:'7134531444980378449L', limit:12, offset:0, sort: 'timestamp:desc' })
+		.then(output => { 
+		
+			//only clear field and display messages when new messages are available
+			if (timestamp!=(output.data[0].timestamp)){
+				$("#messages").html("")
+				//console.log(output);
+				for (var i = 11; i >= 0; i--) {
+					var postMessage=output.data[i].asset.data;
+						postMessage=postMessage.replace(/>/g, "&gt;").replace(/</g, "&lt;"); // /\)\(/g
+						postMessage=postMessage.replace(/\:\)/g, "🙂").replace(/\:\(/g, "☹️"); //allow emoticons
+					 timestamp=output.data[i].timestamp;
+					var postDate=convertTimestamp(timestamp);
+					var postAddress=output.data[i].senderId;
+						
+						$("#messages").append("<p><sup>"+postDate+" - "+postAddress+"</sup><br/>"+postMessage+"</p>");
+				}
 			}
 		}
 	)
